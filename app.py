ca4cf53e1e2e62811fc31f3089e57bf3a1c13520
sashaKorovkina <sasha.korovkina2003@gmail.com>
2025-01-16 13:47:31 +0000

    feat: changed prompt

--- app.py
+++ app.py
@@ -8,13 +8,16 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
+from loguru import logger
+
 
 URL = "https://www.producthunt.com/products/final-round-ai/reviews"
 XPATH = "//div[contains(@class, 'review')]"
 TIMEOUT = 20
 
-st.title("Test Selenium")
+st.title("Product Hunt Reviews")
 st.markdown("You should see some random Football match text below in about 21 seconds")
+
 
 firefoxOptions = Options()
 firefoxOptions.add_argument("--headless")
@@ -29,11 +32,12 @@ try:
     WebDriverWait(driver, TIMEOUT).until(
         EC.visibility_of_element_located((By.XPATH, XPATH,))
     )
+    logger.debug('Successfully discovered elements')
 except TimeoutException:
     st.warning("Timed out waiting for page to load")
     driver.quit()
 
-time.sleep(10)
-elements = driver.find_elements(By.XPATH, XPATH)
-st.write([el.text for el in elements])
-driver.quit()
+# time.sleep(10)
+# elements = driver.find_elements(By.XPATH, XPATH)
+# st.write([el.text for el in elements])
+# driver.quit()
