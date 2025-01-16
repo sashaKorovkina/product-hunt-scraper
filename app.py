69467d04d5a2676d5099fa90598a84b9aca3a727
sashaKorovkina <sasha.korovkina2003@gmail.com>
2025-01-16 13:46:15 +0000

    feat: changed prompt

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import requests
 import streamlit as st
+import time
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
@@ -9,48 +9,31 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
 
-st.title("ProductHunt Review Summaries")
-
-# User input for the link
-XPATH = "//*[@id=\"root-container\"]/div/div[3]/main/div/button"
+URL = "https://www.producthunt.com/products/final-round-ai/reviews"
+XPATH = "//div[contains(@class, 'review')]"
 TIMEOUT = 20
-link = st.text_input("Enter a URL:", placeholder="https://example.com")
 
-if st.button("Fetch Content"):
-    if link:
-        try:
-            # Set up Selenium WebDriver with headless Firefox
-            firefoxOptions = Options()
-            firefoxOptions.add_argument("--headless")
-            service = Service(GeckoDriverManager().install())
-            driver = webdriver.Firefox(
-                options=firefoxOptions,
-                service=service,
-            )
+st.title("Test Selenium")
+st.markdown("You should see some random Football match text below in about 21 seconds")
 
-            # Navigate to the URL
-            driver.get(link)
+firefoxOptions = Options()
+firefoxOptions.add_argument("--headless")
+service = Service(GeckoDriverManager().install())
+driver = webdriver.Firefox(
+    options=firefoxOptions,
+    service=service,
+)
+driver.get(URL)
 
-            # Wait for the button to be visible
-            try:
-                WebDriverWait(driver, TIMEOUT).until(
-                    EC.visibility_of_element_located((By.XPATH, XPATH))
-                )
-                st.success("Page loaded successfully!")
-            except TimeoutException:
-                st.warning("Timed out waiting for the button to load")
-                driver.quit()
+try:
+    WebDriverWait(driver, TIMEOUT).until(
+        EC.visibility_of_element_located((By.XPATH, XPATH,))
+    )
+except TimeoutException:
+    st.warning("Timed out waiting for page to load")
+    driver.quit()
 
-            # Click the button
-            try:
-                button = driver.find_element(By.XPATH, XPATH)
-                button.click()
-                st.success("Button clicked successfully!")
-            except Exception as e:
-                st.error(f"Error clicking the button: {e}")
-                driver.quit()
-        finally:
-            # Clean up the driver
-            driver.quit()
-    else:
-        st.warning("Please enter a valid URL.")
+time.sleep(10)
+elements = driver.find_elements(By.XPATH, XPATH)
+st.write([el.text for el in elements])
+driver.quit()
